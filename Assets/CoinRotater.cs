decc0af3a74fc7c13be8d80e2d584d9ba1848b95
chang47 <coralbue@gmail.com>
2018-06-09 13:25:00 -0700

    fixed unity 2018 update with scenes losing script reference; adding coin asset

---
+++ Assets/CoinRotater.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinRotater : MonoBehaviour {
+	void Update () {
+		transform.Rotate(Vector3.right * 3);
+	}
+}
